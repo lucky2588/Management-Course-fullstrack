bc5aed523e5ab607d7006f8f523e374517ac33ca
lucky2588 <duwcsthang2001@gmail.com>
2023-04-27 04:11:02 +0700

    Create CategoryRepository.java

---
+++ Management Course/src/main/java/com/demo/train/repository/CategoryRepository.java
@@ -0,0 +1,12 @@
+package com.demo.train.repository;
+
+
+
+import com.demo.train.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
+    List<Category> findAll();
+}
