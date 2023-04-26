ed9ecc910a46dc1f055f9e8fea1df185d283c14a
lucky2588 <duwcsthang2001@gmail.com>
2023-04-27 04:10:56 +0700

    Create ManagementCourseApplication.java

---
+++ Management Course/src/main/java/com/demo/train/ManagementCourseApplication.java
@@ -0,0 +1,13 @@
+package com.demo.train;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagementCourseApplication.class, args);
+	}
+
+}
