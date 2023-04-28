3a5e018aa3ca9f5f7b786f62d16426f53cc81cf7
lucky2588 <duwcsthang2001@gmail.com>
2023-04-29 00:21:24 +0700

    Create Layout.jsx

---
+++ Blog-User/src/components/Layout/Layout.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from '../Header/Header'
+import { Outlet } from 'react-router'
+
+function Layout() {
+  return (
+    <>
+       <Header />
+
+       <Outlet />
+
+
+        
+    </>
+  )
+}
+
+export default Layout
