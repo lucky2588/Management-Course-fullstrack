cdd17ab23a19bb5ab2e24816e9277197b6724bde
lucky2588 <duwcsthang2001@gmail.com>
2023-04-29 00:21:27 +0700

    Create main.jsx

---
+++ Blog-User/src/main.jsx
@@ -0,0 +1,22 @@
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux';
+import { store } from './app/store.js';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+  <BrowserRouter>
+  <Provider store={store} >
+  <App />
+  </Provider>
+ 
+  </BrowserRouter>
+  </React.StrictMode>
+
+)
